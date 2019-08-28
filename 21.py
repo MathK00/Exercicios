45e3d5c940dcb855bc33a6019441def5f1ecb98e
Matheus Kaique <54291237+MathK00@users.noreply.github.com>
2019-08-28 20:26:21 -0300

    Delete 21.py

--- 21.py
+++
@@ -1,7 +0,0 @@
-ca = float(input("Digite o cateto adjacente: "))
-co = float(input("Digite o cateto oposto: "))
-h = float(input("Digite a hipotenusa: "))
-if ca + co == h:
-    print("PODEM formar um TRIANGULO RETANGULO")
-else: 
-    print("NÃO PODEM formar um TRIANGULO RETANGULO")    
