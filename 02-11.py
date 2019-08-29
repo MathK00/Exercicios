bba7e5a43e85046d5199b3c3ca16958580fcd5e5
Matheus Kaique <54291237+MathK00@users.noreply.github.com>
2019-08-28 21:45:34 -0300

    Add files via upload

--- 02-11.py
+++ 02-11.py
@@ -1,1 +1,14 @@
-Qual é a velocidade do carro
+v0 = float(input("Insira a velocidade inicial: "))
+a = float(input("Insira a velocidade do automóvel: "))
+t = float(input("Insira o tempo: "))
+v = vo + a * t
+if v <= 40:
+    print("Veículo muito lento")
+elif 40 < V <= 60:
+    print("Velocidade permitida")
+elif 60 < V <= 80
+    print("Velocidade de cruzeiro")
+elif: 80 < V <= 120:
+    print("Veículo rápido")
+else:
+    print("Veículo muito rápido")
